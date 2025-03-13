06f7a86a45516ad0f56ca62ec1bbd3d4dfa53609
EfReeZeey <lol11@Live.ru>
2025-03-13 10:44:07 +0300

    Упразднены DAO, список внедрён в сервис

--- src/main/java/web/services/CarServiceImpl.java
+++ src/main/java/web/services/CarServiceImpl.java
@@ -1,27 +1,30 @@
 package web.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import web.dao.CarDao;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CarServiceImpl implements CarService {
+    private int CAR_COUNTER;
+    private List<Car> cars;
 
-    private final CarDao carDao;
-
-    @Autowired
-    public CarServiceImpl(CarDao carDao) {
-        this.carDao = carDao;
+    public CarServiceImpl() {
+        this.cars = new ArrayList<>();
+        this.cars.add(new Car(++CAR_COUNTER, "Ford", "Fiesta"));
+        this.cars.add(new Car(++CAR_COUNTER, "BMW", "E46"));
+        this.cars.add(new Car(++CAR_COUNTER, "Toyota", "Celica"));
+        this.cars.add(new Car(++CAR_COUNTER, "Ford", "Focus"));
+        this.cars.add(new Car(++CAR_COUNTER, "Ford", "Mustang"));
     }
 
     @Override
     public List<Car> getSomeCars(int count) {
         if (count >= 5) {
-            return carDao.getAllCars();
+            return cars;
         }
-        return carDao.getAllCars().subList(0, count);
+        return cars.subList(0, count);
     }
 }
